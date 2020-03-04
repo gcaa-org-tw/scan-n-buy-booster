b017016a9fd4b54c13420be0ae0a6e2196060897
ddio <ddioibb@gmail.com>
2020-03-04 22:31:50 +0800

    feat: add company info lookup API

---
+++ api/grocery.js
@@ -0,0 +1,152 @@
+const axios = require('axios')
+const qs = require('querystring')
+
+const ENDPOINT = {
+  GS1: 'https://www.gs1tw.org/twct/web/codesearch_send.jsp',
+  RONNY: 'http://company.g0v.ronny.tw/api/search'
+}
+
+async function getGs1 (barcode) {
+
+  // we only accept TW barcode
+  if (barcode.length !== 13 || !barcode.startsWith('471')) {
+    return false
+  }
+  const params = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }
+
+  const resp = await axios.post(
+    ENDPOINT.GS1,
+    qs.stringify({ MCANNO: barcode }),
+    params
+  )
+
+  if (!resp.data) {
+    return false
+  }
+
+  const resultFrom = resp.data.indexOf('此條碼之公司前置碼')
+
+  if (resultFrom < 0) {
+    return false
+  }
+
+  const tailStr = resp.data.slice(resultFrom)
+  const resultTo = tailStr.indexOf('</font>')
+
+  if (resultTo < 0) {
+    return false
+  }
+
+  const result = tailStr.slice(0, resultTo)
+  const isValid = result.indexOf('為有效 GS1 Taiwan 公司前置碼') >= 0
+  const name = result.replace(/.*登記公司名稱為(.+)<P>.*/, '$1')
+
+  if (!isValid) {
+    console.error(`Invalid barcode: ${barcode}`)
+    return false
+  }
+
+  return name.trim()
+}
+
+function getCompanyState (company) {
+  return company.現況 || company.公司狀況
+}
+
+function getCompanyName (company) {
+  return company.公司名稱 || company.商業名稱 || company.名稱
+}
+
+function getCompanyAsset (company) {
+  const keys = ['資本總額(元)', '資本額(元)']
+  let ans = 0
+  keys.some(key => {
+    if (company[key]) {
+      ans = Number(company[key].replace(/,/g, ''))
+      return true
+    }
+  })
+  return ans
+}
+
+function packCompany (company) {
+  return {
+    id: company.統一編號,
+    name: getCompanyName(company)
+  }
+}
+
+async function getCompanyId (targetName) {
+  const query = qs.stringify({
+    q: targetName
+  })
+  const resp = await axios.get(`${ENDPOINT.RONNY}?${query}`)
+  if (!resp.data || !resp.data.data) {
+    return false
+  }
+  const result = resp.data
+
+  if (result.found === 1) {
+    return packCompany(result.data[0])
+  }
+
+  if (result.found < 1) {
+    return false
+  }
+
+  // find only currently active company
+  const activeCompanies = result.data.filter(company => {
+    return getCompanyState(company) === '核准設立'
+  })
+
+  // find the one exactly match query, if not, find largest one
+  let exactMatch = null
+  let largest = null
+
+  activeCompanies.forEach(company => {
+    const thisName = getCompanyName(company)
+    if (thisName === targetName) {
+      exactMatch = company
+    }
+    company.asset = getCompanyAsset(company)
+    if (!largest || largest.asset < company.asset) {
+      largest = company
+    }
+  })
+
+  if (exactMatch) {
+    return packCompany(exactMatch)
+  }
+  return packCompany(largest)
+}
+
+module.exports = async function (req, res) {
+  const ret = {
+    success: false
+  }
+  const barcode = req.params.barcode
+  const companyName = await getGs1(barcode)
+
+  if (!companyName) {
+    ret.reason = 'Invalid barcode'
+    res.json(ret)
+    return
+  }
+
+  const company = await getCompanyId(companyName)
+
+  if (!company) {
+    ret.reason = `Company ${companyName} not found`
+    res.json(ret)
+    return
+  }
+
+  res.json({
+    success: true,
+    data: company
+  })
+}
--- api/server.js
+++ api/server.js
@@ -1,26 +1,36 @@
-const express = require('express');
-const app = express();
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
-const cors = require('cors');
+const express = require('express')
+const app = express()
+const jwt = require('express-jwt')
+const jwksRsa = require('jwks-rsa')
+const cors = require('cors')
+const groceryHandler = require('./grocery')
 
-require('dotenv').config();
+require('dotenv').config()
+
+// setup log
+const Sentry = require('@sentry/node')
+
+if (process.env.SENTRY_DSN) {
+  Sentry.init({ dsn: process.env.SENTRY_DSN })
+  // The request handler must be the first middleware on the app
+  app.use(Sentry.Handlers.requestHandler())
+}
 
 const requiredEnv = [
   'AUTH0_AUDIENCE',
   'AUTH0_DOMAIN',
   'CLIENT_ORIGIN'
-];
+]
 
 if (requiredEnv.some(key => !process.env[key])) {
-  throw 'Make sure you have all required variable in your .env file';
+  throw new Error('Make sure you have all required variable in your .env file')
 }
 
-const corsOptions =  {
+const corsOptions = {
   origin: process.env.CLIENT_ORIGIN
-};
+}
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
 const checkJwt = jwt({
   // Dynamically provide a signing key based on the [Key ID](https://tools.ietf.org/html/rfc7515#section-4.1.4) header parameter ("kid") and the signing keys provided by the JWKS endpoint.
@@ -35,28 +45,21 @@ const checkJwt = jwt({
   audience: process.env.AUTH0_AUDIENCE,
   issuer: `https://${process.env.AUTH0_DOMAIN}/`,
   algorithms: ['RS256']
-});
+})
 
-app.get('/health', function(req, res) {
+app.get('/health', function (req, res) {
   res.json({
     success: true
   })
 })
 
-app.get('/grocery/:barcode', checkJwt, function(req, res) {
-  res.json({
-    success: true,
-    data: {
-      barcode: req.params.barcode
-    }
-  });
-});
+app.get('/grocery/:barcode', checkJwt, groceryHandler)
 
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  return res.status(err.status).json({ message: err.message });
-});
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  return res.status(err.status).json({ message: err.message })
+})
 
 const port = Number(process.env.PORT) || 3001
-app.listen(port);
-console.log(`Listening on http://localhost:${port}`);
+app.listen(port)
+console.log(`Listening on http://localhost:${port}`)
