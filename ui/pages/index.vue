05afa42239b7e4196c6e8a7576cc07441b54b6d7
ddio <ddioibb@gmail.com>
2020-03-10 17:03:35 +0800

    chroe: revise wording

--- ui/pages/index.vue
+++ ui/pages/index.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   main
     .vh-100.vw-100.flex.items-center.justify-center
-      nuxt-link.bg-white.ph3.pv2.ba.br2.db.dim.link.green(to="/create-item/scan") 新增第一筆！
+      nuxt-link.bg-white.ph3.pv2.ba.br2.db.dim.link.green(to="/create-item/scan") 新增一筆！
 </template>
 <script>
 // TODO: show upload count
